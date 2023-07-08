a0514dab4301a5f90fa323b5a2597e88697716a6
jakeogh <github.com@v6y.net>
2023-07-08 11:06:53 -0700

    auto-commit

--- sendgentoo_chroot/sendgentoo_chroot.py
+++ sendgentoo_chroot/sendgentoo_chroot.py
@@ -118,6 +118,7 @@ def rsync_cfg(
         icp(_sendgentoo_post_chroot)
 
         sh.cp(_sendgentoo_post_chroot, "/mnt/gentoo/tmp")
+        sh.chmod("+x", "/mnt/gentoo/tmp/sendgentoo_post_chroot.py")
         sh.cp("/etc/resolv.conf", "/mnt/gentoo/etc")
 
 
